7c180af00b71e42726811e8c21efc90418aaf2ab
guido57 <guido@dogsally.com>
2017-11-01 21:58:55 +0100

    Delete init.lua

--- ESP8266/init.lua
+++
@@ -1,8 +0,0 @@
-
-print("time to rename init.lua into init.old before starting wifi-wps")
-
-tmr.alarm(0, 10000, 1, function ()
-    -- time to rename init.lua into init.old to 
-    dofile("wifi-wps.lua")    
-end)
-
