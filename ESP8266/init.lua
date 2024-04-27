31e9d0fc191c111136feda08c2fadbdd7366c07b
guido <guido@dogsally.com>
2024-04-27 11:02:37 +0200

    deleted ESP8266

--- ESP8266/init.lua
+++
@@ -1,6 +0,0 @@
-print("time to rename init.lua into init.old before starting wifi-wps")
-
-tmr.alarm(0, 3000, 1, function ()
-    -- time to rename init.lua into init.old to 
-    dofile("thermo.lua")    
-end)
